a8406e420e3370d7f7cb1c07c9e4ad736cb0b9e2
yjk-username <1624743653@qq.com>
2022-04-12 14:07:29 +0800

    连接MySQL数据库，django配置数据库

--- musicStudioProject/musicStudioBack/musicStudioBack/asgi.py
+++ musicStudioProject/musicStudioBack/musicStudioBack/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for musicStudioBack project.
+ASGI config for MusicStudio project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicStudioBack.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MusicStudio.settings')
 
 application = get_asgi_application()
--- musicStudioProject/musicStudioBack/musicStudioBack/urls.py
+++ musicStudioProject/musicStudioBack/musicStudioBack/urls.py
@@ -1,4 +1,4 @@
-"""musicStudioBack URL Configuration
+"""MusicStudio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 
+from django.contrib import admin
+from .settings import MEDIA_ROOT
+from django.views.static import serve
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('api-v1/admin/', admin.site.urls),
+    path('api-v1/user/', include('user.urls')),
+    path('api-v1/sign/', include('sign.urls')),
+    path('api-v1/music/', include('music.urls')),
+    path('api-v1/history/', include('history.urls')),
+    path('api-v1/playlist/', include('playlist.urls')),
+    path('api-v1/comment/', include('comment.urls')),
+    path('api-v1/article/', include('article.urls')),
+    path('api-v1/pictest/', include('pictest.urls')),
+    path(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
