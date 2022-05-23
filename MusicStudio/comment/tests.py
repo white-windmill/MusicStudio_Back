b924d69ef60e04bd49fde8ed218570a102428ede
JingkaiYi <1624743653@qq.com>
2022-05-23 18:55:40 +0800

    upgrade comment test.py

--- MusicStudio/comment/tests.py
+++ MusicStudio/comment/tests.py
@@ -1,3 +1,86 @@
 from django.test import TestCase
+from common.models import User
+from common.models import PlayList
+from common.models import Music
+from common.models import PlayListCollection
+    
+import requests
+import unittest
 
-# Create your tests here.
+class UserTest(TestCase):
+
+    def testcase(self):
+
+        User.objects.create(userid="wuyebf",password="gvrg",username="test")
+        qs = User.objects.values()
+        qs = qs.filter(userid="wuyebf")
+        data = list(qs)
+        self.assertEqual(data[0]['username'],"test")
+
+    def setUp(self):
+
+        self.playlist_url = 'http://124.220.169.238:8000/api/playlist/'  
+        self.ret_url = 'http://124.220.169.238:8000/api/playlist/ret/'
+        self.rank_url = 'http://124.220.169.238:8000/api/playlist/rank/'
+        self.comment_url = 'http://124.220.169.238:8000/api/comment/'  
+        self.playlist_test_name = '白色风车'
+        self.comment_test_articleid = '19350102052021-12-4 18:45'
+        self.playlist_test_error_name = '白色1234245'
+        self.playlist_test_userid = '1935010205'
+        self.playlist_test_error_userid = '1935010333'
+        
+    #GET查询userid参数
+    def test_get(self):
+
+        r = requests.get(self.playlist_url+'?playlistname='+self.playlist_test_name)
+        result = r.json()
+        self.assertEqual(result['ret'],0)
+
+        q = requests.get(self.playlist_url+'?playlistname='+self.playlist_test_error_name)
+        result = q.json()
+        self.assertEqual(result['ret'],1)
+
+        rank = requests.get(self.rank_url)
+        result = rank.json()
+        self.assertEqual(result['ret'],0)
+
+        ret = requests.get(self.ret_url+'?userid='+self.playlist_test_userid+'&playlistname='+
+        self.playlist_test_name)
+        result = ret.json()
+        self.assertEqual(result['ret'],0)
+
+        ret = requests.get(self.ret_url+'?userid='+self.playlist_test_error_userid+'&playlistname='+
+        self.playlist_test_error_name)
+        result = ret.json()
+        self.assertEqual(result['ret'],0)
+
+        ret = requests.get(self.comment_url+'?articleid='+self.comment_test_articleid)
+        result = ret.json()
+        self.assertEqual(result['ret'],0)
+
+    def test_post(self):
+        
+        headers = {"content-type":"application/json"}
+        json_data = {'userid':'1935010205','playlistname':'白色风车'}
+        r = requests.post(self.playlist_url,json=json_data,headers=headers)
+        result = r.json()
+        self.assertEqual(result['ret'],0)
+
+        # userId = request.params['userid']
+        # articleId = request.params['articleid']
+        # commentContent = request.params['commentcontent']
+        # commentTime = request.params['commenttime']
+        headers = {"content-type":"application/json"}
+        json_data = {'userid':'1935010205','articleid':self.comment_test_articleid,'commentcontent':'yjkk'
+        ,'commenttime':'2022-1-3 12:12'}
+        r = requests.post(self.comment_url,json=json_data,headers=headers)
+        result = r.json()
+        self.assertEqual(result['ret'],0)
+
+    def test_delete(self):
+
+        headers = {"content-type":"application/json"}
+        json_data = {'userid':'1935010205','playlistname':'白色风车'}
+        r = requests.delete(self.playlist_url,json=json_data,headers=headers)
+        result = r.json()
+        self.assertEqual(result['ret'],0)
